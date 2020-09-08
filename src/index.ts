72afffa8aea886d77b543bab638b8e6adda1e4ce
Vladimir Turcan <vladimir.turcan@sumup.com>
2020-09-08 15:44:41 +0300

    adjust the script

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,47 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application } from 'probot'; // eslint-disable-line no-unused-vars
 
-export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+export default (app: Application) => {
+  // @ts-ignore
+  app.on('pull_request.labeled', async (context) => {
+    const { title: name, body, head } = context.payload.pull_request;
+    const labelName = context.payload.label.name;
+    const { ref } = head;
+    const owner = context.payload.repository.owner.login;
+    const repo = context.payload.repository.name;
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+    if (/releases\/.+/g.test(ref) && labelName === 'bug') {
+      // the convention for creating releases is releases/[year].[week].[releaseNumber]
+      const releaseVersion = ref.split('/')[1];
+      const tag_name = `v${releaseVersion}-01`;
+      app.log('creating a release');
+      try {
+        await context.github.repos
+          .createRelease({
+            repo,
+            owner,
+            tag_name,
+            target_commitish: ref,
+            name,
+            body,
+            draft: false,
+            prerelease: false,
+          })
+          .then(() => {
+            app.log('release successfully created');
+            const prReview = context.pullRequest({
+              body:
+                'Github Release successfully created. Deploy on staging is on the way.',
+            });
+            app.log('writing a review for the PR');
+            return context.github.pulls.createReview({
+              ...prReview,
+              event: 'APPROVE',
+            });
+          });
+        app.log('finished script');
+      } catch (error) {
+        app.log(error);
+      }
+    }
+  });
+};
