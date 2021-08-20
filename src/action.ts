70f8ca39376ece04b3a0caca7a3dfa97e1259022
Vladimir Turcan <vladimir.turcan@sumup.com>
2021-08-20 15:18:20 +0300

    prepare for turning the app into a github action

---
+++ src/action.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+const { run } = require('@probot/adapter-github-actions')
+const app = require("./app");
+
+run(app).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
